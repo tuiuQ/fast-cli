4c321e625900ae4bb591e85ea301d74652163960
tuiu <13719283454@163.com>
2025-07-14 19:30:24 +0800

    command inti

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-console.log("hello fast-cli");
+import { Command } from "commander"
+import pkg from "../package.json"
+
+const program = new Command("fast")
+
+program.version(pkg.version)
+
+program.parse(process.argv)
