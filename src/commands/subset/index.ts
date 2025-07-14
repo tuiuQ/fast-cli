8764f9e4b30475766d27fe2c6356f13fec192823
tuiu <13719283454@163.com>
2025-07-15 02:17:43 +0800

    feat(commands): 添加subset命令支持字体子集化功能
    添加新的subset命令用于处理字体子集化，支持指定语言和格式选项

---
+++ src/commands/subset/index.ts
@@ -0,0 +1,18 @@
+import type { Command } from "commander";
+
+interface ISubsetOptions {
+  lang: string;
+  format: string;
+}
+
+export function setupSubsetCommand(program: Command) {
+  program
+    .command("subset")
+    .description("subset dist folder")
+    .argument("<font_file>", "font file path")
+    .option("-l, --lang <lang>", "subset lang")
+    .option("-f, --format <format>", "subset format")
+    .action((file: string, options: ISubsetOptions) => {
+      // TODO: subset font file
+    });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { Command } from "commander"
 import pkg from "../package.json"
 import { setupCleanCommand } from "./commands/clean"
+import { setupSubsetCommand } from "./commands/subset"
 
 const program = new Command("fast")
 
 program.version(pkg.version)
 
 setupCleanCommand(program)
+setupSubsetCommand(program)
 
 program.parse(process.argv)
