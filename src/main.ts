42bc91c63ed388988b00e25970f61dd0b4e29eb5
tuiu <13719283454@163.com>
2025-07-18 01:27:24 +0800

    docs(main): 添加主程序入口文件的详细注释
    添加对主入口文件的详细注释说明，包括程序功能概述和各个主要部分的用途说明

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,36 @@
+/**
+ * Fast CLI 主入口文件
+ * 
+ * 提供字体子集化和清理功能的命令行工具
+ * 
+ * @fileoverview 主程序入口，负责初始化命令行界面和注册所有子命令
+ */
+
 import { Command } from "commander"
 import pkg from "../package.json"
 import { setupCleanCommand } from "./commands/clean"
 import { setupSubsetCommand } from "./commands/subset"
 
+/**
+ * 创建并配置CLI程序
+ */
 const program = new Command("fast")
 
-program.version(pkg.version)
+// 设置程序版本信息
+program.version(pkg.version, '-v, --version', '显示版本号')
 
+/**
+ * 注册所有子命令
+ */
 setupCleanCommand(program)
 setupSubsetCommand(program)
 
+/**
+ * 解析并执行命令
+ */
 program.parse(process.argv)
+
+// 如果没有提供命令，显示帮助信息
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+}
