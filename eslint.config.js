38e242654e4c5250d2b2f2b25f91eb35cd3915d7
tuiu <13719283454@163.com>
2025-07-18 03:44:22 +0800

    feat: 引入 Prettier，用于代码格式化

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import eslintPluginTypeScript from '@typescript-eslint/eslint-plugin';
 import eslintParserTypeScript from '@typescript-eslint/parser';
 import globals from 'globals';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   {
@@ -18,12 +20,17 @@ export default [
     },
     plugins: {
       '@typescript-eslint': eslintPluginTypeScript,
+      prettier: eslintPluginPrettier,
     },
     rules: {
       ...eslintPluginTypeScript.configs['eslint-recommended'].rules,
       ...eslintPluginTypeScript.configs.recommended.rules,
       '@typescript-eslint/no-explicit-any': 'off',
+      'prettier/prettier': 'off',
       // Add custom rules here if needed
     },
   },
+  eslintConfigPrettier,
+];
+  eslintConfigPrettier,
 ];
