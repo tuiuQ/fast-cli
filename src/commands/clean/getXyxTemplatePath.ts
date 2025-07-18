b31b30461c2fe915ca1164a350fa217f4dfc7419
tuiu <13719283454@163.com>
2025-07-18 11:27:35 +0800

    feat: 添加获取 xyx 模板路径的工具函数
    此提交引入了 ，它提供了一个异步函数，用于根据 xyx 模板的版本构建完整的模板路径。这有助于  命令定位和管理 xyx CLI 模板文件。

---
+++ src/commands/clean/getXyxTemplatePath.ts
@@ -0,0 +1,8 @@
+import { join } from "path";
+import { xyxTemplatePath } from "./constants";
+import { getXyxTemplateVersion } from "./getXyxTemplateVersion";
+
+export async function getXyxTemplatePath(filePath: string) {
+  const version = await getXyxTemplateVersion();
+  return join(xyxTemplatePath, `xyx-template-${version}`);
+}
